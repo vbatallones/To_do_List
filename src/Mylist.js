d459e6075b6ec5a9b7491d383c85d80547c288ed
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-26 10:37:01 -0700

    [Mylist] add Mylist components

---
+++ src/Mylist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class Mylist extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This I should stop procrastinating:</h1>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Mylist
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Mylist from './Mylist'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Mylist />
   </React.StrictMode>,
   document.getElementById('root')
 );
